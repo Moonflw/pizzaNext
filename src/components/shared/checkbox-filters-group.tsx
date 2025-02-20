0983817f4eeaa9df91f915f0650e540abfa6001a
Munir Safin <munirsafin@MacBook-Air-Munir.local>
2025-02-20 04:04:05 -0800

    1hour 46min

--- src/components/shared/checkbox-filters-group.tsx
+++ src/components/shared/checkbox-filters-group.tsx
@@ -1,25 +1,61 @@
+'use client'
 import React from 'react';
-import { cn } from '@/lib/utils';
-import { FilterChecboxProps } from './filtercheckbox';
+import { FilterChecboxProps, FilterCheckbox } from './filtercheckbox';
+import { Input } from '../ui';
 
 type Item = FilterChecboxProps
 
 interface Props {
-    title: string;
-    items: Item[];
-    defaultItems?: Item[];
-    limit?: number;
-    loading?: boolean;
-    searchInputPlaceholder?: string;
-    onClickCheckbox?: (id: string) => void;
-    defaultValue?: string[];
-    selected?: Set<string>;
-    className?: string;
-    name?: string;
-  }
+  title: string;
+  items: Item[];
+  defaultItems?: Item[];
+  limit?: number;
+  loading?: boolean;
+  searchInputPlaceholder?: string;
+  onClickCheckbox?: (id: string) => void;
+  defaultValue?: string[];
+  selected?: Set<string>;
+  className?: string;
+  name?: string;
+}
 
-export const CheckboxFiltersGroup: React.FC<Props> = ({ className }) => {
+export const CheckboxFiltersGroup: React.FC<Props> = ({
+  title,
+  items,
+  defaultItems,
+  limit = 5,
+  searchInputPlaceholder = 'Поиск...',
+  className,
+  loading,
+  onClickCheckbox,
+  selected,
+  name,
+}) => {
+  const [showAll, setShowAll] = React.usetState(false)
   return (
-    <div className={cn(className)}></div>
+
+    <div className={className}>
+      <p className="font-bold mb-3">{title}</p>
+
+      {showAll && (
+          <div className='mb-5'>
+            <Input placeholder={searchInputPlaceholder} className='bg-gray-50 border-none' />
+          </div>
+        )}
+
+      <div className="flex flex-col gap-4 max-h-96 pr-2 overflow-auto scrollbar">
+        {items.map((item, index) => (
+          <FilterCheckbox
+            key={index}
+            text={item.text}
+            value={item.value}
+            endAdornment={item.endAdornment}
+            checked={false}
+            onCheckedChange={(ids) => console.log(ids)}
+          />
+        ))}
+      </div>
+
+    </div>
   );
 };
--- src/components/shared/filters.tsx
+++ src/components/shared/filters.tsx
@@ -4,6 +4,7 @@ import { Title } from './title';
 import { FilterCheckbox } from './filtercheckbox';
 import { Input } from '../ui';
 import { RangeSlider } from './range-slider';
+import { CheckboxFiltersGroup } from './checkbox-filters-group';
 
 interface Props {
     className?: string;
@@ -14,10 +15,14 @@ export const Filters: React.FC<Props> = ({ className }) => {
         <div className={cn(className)}>
             <Title text="Фильтрация" size="sm" className="mb-5 font-bold" />
 
+            {/* top Checkbox */}
+
             <div className='flex flex-col gap-4'>
                 <FilterCheckbox text="Можно собирать" value="1" />
                 <FilterCheckbox text="Новинки" value="2" />
             </div>
+
+            {/* filter price */}
 
             <div className="mt-5 border-y border-y-neutral-100 py-6 pb-7">
                 <p className="font-bold mb-3">Цена от и до:</p>
@@ -42,6 +47,71 @@ export const Filters: React.FC<Props> = ({ className }) => {
                     step={10}
                 />
             </div>
+            <CheckboxFiltersGroup
+        title='Ingridients'
+        className='mt-5'
+        limit={6} 
+        defaultItems={[
+          {
+            text: "cheese souce",
+            value: "1",
+          },
+          {
+            text: "Mozzarella",
+            value: "2",
+          },
+          {
+            text: "Onion",
+            value: "3",
+          },
+          {
+            text: "souce",
+            value: "4",
+          },
+          {
+            text: "cheese",
+            value: "5",
+          },
+          {
+            text: "tomato",
+            value: "6",
+          },
+          {
+            text: "potato",
+            value: "7",
+          },
+        ]}
+        items={[
+          {
+            text: "cheese souce",
+            value: "1",
+          },
+          {
+            text: "Mozzarella",
+            value: "2",
+          },
+          {
+            text: "Onion",
+            value: "3",
+          },
+          {
+            text: "souce",
+            value: "4",
+          },
+          {
+            text: "cheese",
+            value: "5",
+          },
+          {
+            text: "tomato",
+            value: "6",
+          },
+          {
+            text: "potato",
+            value: "7",
+          },
+        ]}
+      />
         </div>
     );
 };
